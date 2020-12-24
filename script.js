9fccab57422c60db1afbc237f5a5431081a9b994
Yoonjoo Park <yoonjoo16@gmail.com>
2020-12-24 11:49:36 +0100

    fixed mergeLeft

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var board = Array(Array(0, 0, 0, 0), Array(0, 0, 0, 0), Array(0, 0, 0, 0), Array(0, 0, 0, 0));
+var board = Array(Array(0, 2, 0, 4), Array(2, 2, 4, 4), Array(0, 2, 4, 2), Array(0, 0, 0, 2));
 
 //start game
 initBoard();
@@ -16,7 +16,7 @@ function checkKey(e) {
 
 function initBoard() {
     //initialize the board when new game starts.
-    for (var i = 0; i < 4; i++) {
+  /*  for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             board[i][j] = 0;
         }
@@ -27,7 +27,7 @@ function initBoard() {
         var y = parseInt(Math.random() * 4);
         if (board[x][y] == 0) board[x][y] = 2;
         else i++;
-    }
+    }*/
     draw();
 }
 
@@ -37,6 +37,7 @@ function draw() {
         for (var j = 0; j < 4; j++) {
             var cell = document.getElementById("board").rows[i].cells[j];
             if(board[i][j] != 0) cell.innerHTML = board[i][j];
+            else cell.innerHTML = "";
         }
     }
 }
@@ -44,11 +45,40 @@ function draw() {
 //move as the direction and add the numbers
 function move(direction) {
     switch(direction) {
-        case "left": 
+        case "left": board = mergeLeft();
         case "right":
         case "up":
         case "right":
     }
+
+    draw();
 }
 
+function mergeLeft() {
+    var temp = Array(Array(0, 0, 0, 0), Array(0, 0, 0, 0), Array(0, 0, 0, 0), Array(0, 0, 0, 0));
 
+    for(var i = 0; i < 4; i++) {
+        temp[i] = moveNumbersToLeft(board[i]);
+        for(var j=0; j<3; j++) {
+            if(temp[i][j] == temp[i][j+1]) {
+                temp[i][j] = temp[i][j] * 2;
+                temp[i][j+1] = 0;
+                temp[i] = moveNumbersToLeft(temp[i]);
+            }
+        }        
+    }
+    return temp;
+
+}
+
+function moveNumbersToLeft(array) {
+    var temp = Array(0,0,0,0);
+    var i = 0;
+    for(var j=0; j<4; j++) {
+       if(array[j] != 0) {
+           temp[i] = array[j];
+           i++;
+       }
+   }
+   return temp;
+}
